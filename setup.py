7bff7cf76e329813b37cbc262d948a870b21ecdf
Ludovic Laurent <ludovic.laurent@synapse.local>
2025-01-06 14:18:53 +0100

    update version packages

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
-from distutils.version import LooseVersion
+# from distutils.version import LooseVersion
 import os
 import sys
 
-from setuptools import __version__ as setuptools_version
+# from setuptools import __version__ as setuptools_version
 from setuptools import find_packages
 from setuptools import setup
 
@@ -13,8 +13,7 @@ version = '3.1.0.dev0'
 install_requires = [
     'setuptools',
     'zope.interface',
-    'requests>=2.4.2',
-    'dns-lexicon>=3.15.1'
+    'requests>=2.32.3',
 ]
 
 if not os.environ.get('SNAP_BUILD'):
@@ -28,14 +27,14 @@ elif 'bdist_wheel' in sys.argv[1:]:
 if os.environ.get('SNAP_BUILD'):
     install_requires.append('packaging')
 
-setuptools_known_environment_markers = (LooseVersion(setuptools_version) >= LooseVersion('36.2'))
-if setuptools_known_environment_markers:
-    install_requires.append('mock ; python_version < "3.3"')
-elif 'bdist_wheel' in sys.argv[1:]:
-    raise RuntimeError('Error, you are trying to build certbot wheels using an old version '
-                       'of setuptools. Version 36.2+ of setuptools is required.')
-elif sys.version_info < (3,3):
-    install_requires.append('mock')
+# setuptools_known_environment_markers = (LooseVersion(setuptools_version) >= LooseVersion('36.2'))
+# if setuptools_known_environment_markers:
+#     install_requires.append('mock ; python_version < "3.3"')
+# elif 'bdist_wheel' in sys.argv[1:]:
+#     raise RuntimeError('Error, you are trying to build certbot wheels using an old version '
+#                        'of setuptools. Version 36.2+ of setuptools is required.')
+# elif sys.version_info < (3,3):
+#     install_requires.append('mock')
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
